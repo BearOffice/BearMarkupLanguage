2d379826ac81560be595791f71a5a8120522d61b
Misaki Bear <51944655+MisakiBear@users.noreply.github.com>
2020-10-11 23:12:31 +0900

    Trim code

--- ConfReaderLib/ConfReader.cs
+++ ConfReaderLib/ConfReader.cs
@@ -1,9 +1,7 @@
-﻿using System;
 using System.IO;
 using System.Linq;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
-using System.Reflection;
 
 namespace ConfReaderLib
 {
